27498e502b0a3365a5cf002b25054ff18d790d7f
Hannes <hannes_rauhe@yahoo.de>
2012-07-01 13:54:55 +0200

    *generate arguments...

--- creator/generate.py
+++ creator/generate.py
@@ -2,7 +2,7 @@ import argparse
 from scihog.creator import *
 
 parser = argparse.ArgumentParser(description='Create HTML-Files from Template.')
-parser.add_argument('template', type=str, default='../__template.html',
+parser.add_argument('--template', type=str, default='../__template.html',
                    help='the template file and output-dir')
 
 args = parser.parse_args()
