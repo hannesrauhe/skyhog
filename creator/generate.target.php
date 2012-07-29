b894e13956b44357ea025e3391f2508a4a9f127b
Hannes <hannes_rauhe@yahoo.de>
2012-07-29 23:12:58 +0200

    *finalizing-option

--- creator/generate.target.php
+++ creator/generate.target.php
@@ -2,8 +2,12 @@
 require_once('./base.inc.php');
 
 $arg1 = escapeshellarg(dirname(__FILE__)."/generate.py");
+$arg2 = "";
+if(array_key_exists("finalize", $_POST)) {
+	$arg2 = "--final";
+}
 $retvar = 0;
-$ret = system(PYTHON_CMD." $arg1",$retvar);
+$ret = system(PYTHON_CMD." $arg1 $arg2",$retvar);
 if($ret === FALSE || $retvar!=0) {
     echo "ERROR executing python\n";
     exit();
