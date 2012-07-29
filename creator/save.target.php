62c067632c2406d48edb44b2d417ec686360f49d
Hannes <hannes_rauhe@yahoo.de>
2012-07-29 20:39:47 +0200

    *bugfix

--- creator/save.target.php
+++ creator/save.target.php
@@ -19,10 +19,10 @@ if(array_key_exists('elm1',$_POST) && !empty($_POST['elm1'])) {
 	exit();
 }
 
-$gitarg1 = escapeshellarg(UPLOAD_DIR.".git");
-$gitarg2 = escapeshellarg(UPLOAD_DIR.$file);
+chdir(UPLOAD_DIR);
+$gitarg1 = escapeshellarg($file);
 $retvar = 0;
-$ret = system(GIT_CMD." --git-dir=$gitarg1 add $gitarg2",$retvar);
+$ret = system(GIT_CMD." add $gitarg1",$retvar);
 if($ret === FALSE || $retvar!=0) {
     echo "ERROR: adding $file with git wasn't possible\n";
 	echo $ret;
@@ -30,7 +30,7 @@ if($ret === FALSE || $retvar!=0) {
 }
 $gitarg2 = escapeshellarg("Commit from webinterface, IP:".$_SERVER["REMOTE_ADDR"]);
 $retvar = 0;
-$ret = system(GIT_CMD." --git-dir=$gitarg1 commit -m $gitarg2",$retvar);
+$ret = system(GIT_CMD." commit -m $gitarg1",$retvar);
 if($ret === FALSE || $retvar!=0) {
     echo "ERROR: commiting staged $file with git wasn't possible, commit-msg was $gitarg\n";
 	echo $ret;
