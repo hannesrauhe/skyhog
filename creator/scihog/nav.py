cfbe809c1fa03f4f8010cd13a4c09ee0cb7b5be5
unknown <hannes@.(none)>
2012-09-18 23:30:36 +0200

    *forgot something in last bugfix

--- creator/scihog/nav.py
+++ creator/scihog/nav.py
@@ -7,7 +7,7 @@ def generate(generator_class,content_name):
     code.append('<nav>\n')
     code.append('<ul>\n')
     for entry in cursor.fetchall():
-        if content_name==entry[0]:
+        if content_name==entry[1]:
             code.append('<li id="%s" class="m_active"><a href="%s">%s</a></li>\n' % entry[:3])
         else:
             code.append('<li id="%s"><a href="%s">%s</a></li>\n' % entry[:3])
